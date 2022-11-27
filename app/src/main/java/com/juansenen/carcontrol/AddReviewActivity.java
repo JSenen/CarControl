af3c2a2fdacd4cabbdf17990bb7e432da61045bd
Juan Senen <juansenen@hotmail.com>
2022-11-27 17:08:11 +0100

    Añadido DatePickerFragment para campo fechas en AddReviewActivity

--- app/src/main/java/com/juansenen/carcontrol/AddReviewActivity.java
+++ app/src/main/java/com/juansenen/carcontrol/AddReviewActivity.java
@@ -6,12 +6,14 @@ import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.room.Room;
 
+import android.app.DatePickerDialog;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.CheckBox;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -19,10 +21,12 @@ import android.widget.Toast;
 import com.juansenen.carcontrol.db.AppDatabase;
 import com.juansenen.carcontrol.domain.Fuel;
 import com.juansenen.carcontrol.domain.Reviews;
+import com.juansenen.carcontrol.util.DatePickerFragment;
 
-public class AddReviewActivity extends AppCompatActivity {
+public class AddReviewActivity extends AppCompatActivity implements View.OnClickListener {
 
     private String matricula;
+    private EditText editTFecha;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -36,10 +40,14 @@ public class AddReviewActivity extends AppCompatActivity {
         TextView txtregister = findViewById(R.id.txt_review_register);
         txtregister.setText(matricula);
 
+        editTFecha = findViewById(R.id.edt_addreview_date);
+        editTFecha.setOnClickListener(this);
+
     }
+
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        getMenuInflater().inflate(R.menu.menu_back,menu);
+        getMenuInflater().inflate(R.menu.menu_back, menu);
         return true;
     }
 
@@ -50,9 +58,8 @@ public class AddReviewActivity extends AppCompatActivity {
         return true;
     }
 
-    public void butAddReview(View view){
+    public void butAddReview(View view) {
         TextView txtRegister = findViewById(R.id.txt_review_register);
-        EditText edtReviewDate = findViewById(R.id.edt_addreview_date);
         EditText edtReviewKm = findViewById(R.id.edt_addreview_km);
         EditText edtReviewPrice = findViewById(R.id.edt_addreview_price);
         CheckBox chkReviewOil = findViewById(R.id.chk_addreview_oil);
@@ -63,7 +70,7 @@ public class AddReviewActivity extends AppCompatActivity {
         CheckBox chkReviewWipers = findViewById(R.id.chk_addreview_wipers);
 
         String reviewRegister = txtRegister.getText().toString();
-        String reviewDate = edtReviewDate.getText().toString();
+        String reviewDate = editTFecha.getText().toString();
         int reviewKm = Integer.parseInt(edtReviewKm.getText().toString());
         float reviewPrice = Float.parseFloat(edtReviewPrice.getText().toString());
         boolean reviewOil = chkReviewOil.isChecked();
@@ -73,18 +80,18 @@ public class AddReviewActivity extends AppCompatActivity {
         boolean reviewWheels = chkReviewWheels.isChecked();
         boolean reviewWipers = chkReviewWipers.isChecked();
 
-        Reviews review = new Reviews(reviewRegister,reviewDate,reviewKm,reviewPrice,reviewOil,
-                reviewBrakes,reviewFreeze,reviewLiqBrakes,reviewWheels,reviewWipers);
+        Reviews review = new Reviews(reviewRegister, reviewDate, reviewKm, reviewPrice, reviewOil,
+                reviewBrakes, reviewFreeze, reviewLiqBrakes, reviewWheels, reviewWipers);
 
-        Reviews reviews = new Reviews(reviewRegister,reviewDate,reviewKm,reviewPrice,reviewOil
-                ,reviewBrakes,reviewFreeze,reviewLiqBrakes,reviewWheels,reviewWipers);
-        final AppDatabase db = Room.databaseBuilder(this,AppDatabase.class, DATABASE_NAME)
+        Reviews reviews = new Reviews(reviewRegister, reviewDate, reviewKm, reviewPrice, reviewOil
+                , reviewBrakes, reviewFreeze, reviewLiqBrakes, reviewWheels, reviewWipers);
+        final AppDatabase db = Room.databaseBuilder(this, AppDatabase.class, DATABASE_NAME)
                 .allowMainThreadQueries().build();
         db.reviewDAO().insert(reviews);
 
-        Toast.makeText(this, R.string.add,Toast.LENGTH_SHORT).show();
+        Toast.makeText(this, R.string.add, Toast.LENGTH_SHORT).show();
 
-        edtReviewDate.setText("");
+        editTFecha.setText("");
         edtReviewKm.setText("");
         edtReviewPrice.setText("");
         chkReviewOil.setChecked(false);
@@ -94,5 +101,30 @@ public class AddReviewActivity extends AppCompatActivity {
         chkReviewWheels.setChecked(false);
         chkReviewWipers.setChecked(false);
 
+    }
+
+    //Metodo onclick EditText fecha
+    @Override
+    public void onClick(View view) {
+        switch (view.getId()) {
+            case R.id.edt_addreview_date:
+                showDatePickerDialog();
+                break;
+            default:
+
+        }
+    }
+    //Mostrar Calendario al pulsar sobre edittext fecha
+    private void showDatePickerDialog() {
+        DatePickerFragment newFragment = DatePickerFragment.newInstance(new DatePickerDialog.OnDateSetListener() {
+            @Override
+            public void onDateSet(DatePicker datePicker, int year, int month, int day) {
+                // +1 Por que Enero es 0
+                final String selectedDate = day + " / " + (month+1) + " / " + year;
+                editTFecha.setText(selectedDate);
+            }
+        });
+
+        newFragment.show(getSupportFragmentManager(), "datePicker");
     }
 }
